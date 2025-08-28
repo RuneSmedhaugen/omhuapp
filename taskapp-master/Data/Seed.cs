068a6c4ca287948c9c668ef46722503f8a5e7591
RuneSmedhaugen <157691381+RuneSmedhaugen@users.noreply.github.com>
2025-08-28 12:30:20 +0200

    test

--- taskapp-master/Data/Seed.cs
+++ taskapp-master/Data/Seed.cs
@@ -1,12 +1,40 @@
+using System.Net.Http;
 using System.Threading.Tasks;
-using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using TaskApp.API.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace TaskApp.API.Data {
-    public class Seed {
+namespace TaskApp.API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context)
+        {
+            if (await context.Employees.AnyAsync()) return;
 
-        public static Task SeedData(DataContext context) {
-            /* Implement method to fetch data from https://omhuapp.no/api/data.json */
-            throw new NotImplementedException();
+            using var client = new HttpClient();
+            var json = await client.GetStringAsync("https://omhuapp.no/api/data.json");
+
+            // Emplyee list
+            var employees = JsonConvert.DeserializeObject<List<Employee>>(json);
+
+            // Patiens list
+            var patients = new List<Patient>();
+            foreach (var employee in employees)
+            {
+                if (employee.Patients != null)
+                {
+                    foreach (var patient in employee.Patients)
+                    {
+                        patients.Add(patient);
+                    }
+                }
+            }
+
+            context.Employees.AddRange(employees);
+            context.Patients.AddRange(patients);
+            await context.SaveChangesAsync();
         }
     }
 }
